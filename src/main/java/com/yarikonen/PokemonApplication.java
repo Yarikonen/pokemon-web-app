ecde8f056badaaf9970ad6e81cd1932eb63dcf3d
Yarikonen <67190436+Yarikonen@users.noreply.github.com>
2023-03-21 20:04:16 +0300

    postgresql connection added

--- src/main/java/com/yarikonen/PokemonApplication.java
+++ src/main/java/com/yarikonen/PokemonApplication.java
@@ -1,4 +1,4 @@
-package com.example.pokemon;
+package com.yarikonen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/yarikonen/PokemonApplicationTests.java
+++ src/test/java/com/yarikonen/PokemonApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.pokemon;
+package com.yarikonen;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
